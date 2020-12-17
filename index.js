2b4ca9d2137f83abcfef8115bff78a6833850563
Tedwar52 <67608987+tedwar52@users.noreply.github.com>
2020-12-17 15:09:45 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [{
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Despcription of your project:"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the installation instructions?"
+    },
+    {
+        type: "input",
+        name: "guidelines",
+        message: "What are the contribution guidelines?"
+    },
+    {
+        type: "input",
+        name: "testing",
+        message: "What are the test instructions?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Which license is the application covered under?",
+        choices: ["x", "y", "z"]
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    }
+];
+questions.forEach(response => {
+    inquirer.prompt(questions)
+        .then(answers => {
+            const response = answers
+        })
+        .catch(error => {
+            if (error)
+                return "Try Again!"
+        })
+});
+
+// function to write README file
+let input = {
+    x,
+    y,
+    ...process.argv
+}
+
+//function writeToFile(filename, data) {
+fs.writeFile("readme.md", input, function (err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("Data Saved!")
+})
+
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
