a32c9b80748b24e33f4e0fef250526c779ec0bd6
Tedwar52 <67608987+tedwar52@users.noreply.github.com>
2020-12-17 17:08:28 -0500

    updated generatemarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,36 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  ##${data.title}##
+
+  ##_Description_**
+
+  ${data.description}
+
+  ##_Installation_##
+
+  ${data.installation}
+
+  ##_Usage_##
+
+  ${data.usage}
+
+  ##_Guidelines_##
+
+  ${data.guidelines}
+
+  ##_Testing_##
+
+  ${data.testing}
+
+  ##_License_##
+
+  ${data.license}
+
+  ##_See more about this_##
+
+  Github link: ${data.github},
+  Email Address: ${data.email}
 
 `;
 }
